d4ddd80a6bf7d35e08b74f7281de0581a47618f3
Not-Minimal <sam.pedreros14@gmail.com>
2025-10-19 17:59:33 -0300

    Se suben ejercicios

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,37 @@
 fn main() {
-
-#[derive(Debug)]
-enum RolUsuario {
-    Admin, 
-    Moderador,
-    Usuario,
-    Invitado,
-}
-struct Usuario {
-    nombre: String,
-    email: String,
-    activo: bool,
-    contador_inicio_sesion: u64,
-    rol: RolUsuario,
-}
+    #[derive(Debug)]
+    enum RolUsuario {
+        Admin,
+        Moderador,
+        Usuario,
+        Invitado,
+    }
+    struct Usuario {
+        nombre: String,
+        email: String,
+        activo: bool,
+        contador_inicio_sesion: u64,
+        rol: RolUsuario,
+    }
     impl Usuario {
- fn imprimir_rol(&self) {
-    match self.rol {
-        RolUsuario::Admin => {
-            println!("El usuario: {}, es un Administrador", &self.nombre);
-        }
-        RolUsuario::Usuario => {
-            println!("El usuario: {}, es un Usuario", &self.nombre);
-        }
-        RolUsuario::Moderador => {
-            println!("El usuario: {}, es un Moderador", &self.nombre);
-        }
-        RolUsuario::Invitado => {
-            println!("El usuario: {}, es un Invitado", &self.nombre);
+        fn imprimir_rol(&self) {
+            match self.rol {
+                RolUsuario::Admin => {
+                    println!("El usuario: {}, es un Administrador", &self.nombre);
+                }
+                RolUsuario::Usuario => {
+                    println!("El usuario: {}, es un Usuario", &self.nombre);
+                }
+                RolUsuario::Moderador => {
+                    println!("El usuario: {}, es un Moderador", &self.nombre);
+                }
+                RolUsuario::Invitado => {
+                    println!("El usuario: {}, es un Invitado", &self.nombre);
+                }
+            }
         }
     }
-}       
-    }
-    let usuario1 = Usuario{
+    let usuario1 = Usuario {
         nombre: "Saul Muñoz".to_string(),
         email: "sam@gmail.com".to_string(),
         activo: true,
@@ -40,21 +39,21 @@ struct Usuario {
         rol: RolUsuario::Usuario,
     };
 
-    let usuario2 = Usuario{
+    let usuario2 = Usuario {
         nombre: "Saul Muñon".to_string(),
         email: "sam.pedreros@gmail.com".to_string(),
         activo: true,
         contador_inicio_sesion: 2,
         rol: RolUsuario::Admin,
     };
-    let usuario3 = Usuario{
+    let usuario3 = Usuario {
         nombre: "Saul Muñon".to_string(),
         email: "sam.pedreros@gmail.com".to_string(),
         activo: true,
         contador_inicio_sesion: 3,
         rol: RolUsuario::Invitado,
     };
-    let usuario4 = Usuario{
+    let usuario4 = Usuario {
         nombre: "Saul Muñon".to_string(),
         email: "sam.pedreros@gmail.com".to_string(),
         activo: true,
@@ -72,4 +71,3 @@ struct Usuario {
     println!("Inicio de Sesion: {}", usuario1.contador_inicio_sesion);
     println!("Rol: {:?}", usuario1.rol);
 }
-
